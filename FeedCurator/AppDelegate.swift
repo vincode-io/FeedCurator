02be73e4e77bf276bdcd9562bd829b4fcc7be1b9
Maurice Parker <mo@vincode.io>
2019-04-03 10:28:43 -0500

    Changed version to 1.0 and fleshed out the Help menu.

--- FeedCurator/AppDelegate.swift
+++ FeedCurator/AppDelegate.swift
@@ -49,4 +49,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 			
 	}
 
+	@IBAction func showHelp(_ sender: Any?) {
+		MacWebBrowser.openURL(URL(string: "https://vincode.io/feed-curator-help/")!, inBackground: false)
+	}
+
+	@IBAction func showWebsite(_ sender: Any?) {
+		MacWebBrowser.openURL(URL(string: "https://vincode.io/feed-curator/")!, inBackground: false)
+	}
+	
+	@IBAction func showGithubRepo(_ sender: Any?) {
+		MacWebBrowser.openURL(URL(string: "https://github.com/vincode-io/FeedCurator")!, inBackground: false)
+	}
+	
+	@IBAction func showGithubIssues(_ sender: Any?) {
+		MacWebBrowser.openURL(URL(string: "https://github.com/vincode-io/FeedCurator/issues")!, inBackground: false)
+	}
+	
 }
