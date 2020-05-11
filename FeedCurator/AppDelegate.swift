eca08fbaecb4efdcbeefac0bb6ed0a3bce9278d7
Maurice Parker <mo@vincode.io>
2020-05-11 04:19:31 -0500

    Remove the last bit of Octokit

--- FeedCurator/AppDelegate.swift
+++ FeedCurator/AppDelegate.swift
@@ -2,15 +2,11 @@
 
 import AppKit
 import RSWeb
-import OctoKit
 
 var appDelegate: AppDelegate!
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-	var githubOAuthConfig: OAuthConfiguration?
-	var githubTokenConfig: TokenConfiguration?
 
 	override init() {
 		super.init()
@@ -18,37 +14,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	}
 	
 	func applicationWillFinishLaunching(_ notification: Notification) {
-		NSAppleEventManager.shared().setEventHandler(self, andSelector: #selector(AppDelegate.handleCallback(_:_:)), forEventClass: AEEventClass(kInternetEventClass), andEventID: AEEventID(kAEGetURL))
 	}
 
 	func applicationDidFinishLaunching(_ notification: Notification) {
-		githubOAuthConfig = OAuthConfiguration(token: Secrets.GitHub.clientId, secret: Secrets.GitHub.clientSecret, scopes: ["repo", "gist"])
 	}
 	
-	@objc func handleCallback(_ event: NSAppleEventDescriptor, _ withReplyEvent: NSAppleEventDescriptor) {
-		
-		guard let urlString = event.paramDescriptor(forKeyword: keyDirectObject)?.stringValue else {
-			return
-		}
-
-		guard let url = URL(string: urlString) else {
-			return
-		}
-		
-		githubOAuthConfig?.handleOpenURL(url: url) { [unowned self] tokenConfig in
-			
-			self.githubTokenConfig = tokenConfig
-			
-			DispatchQueue.main.async {
-				NSApplication.shared.windows.forEach {
-					$0.toolbar?.validateVisibleItems()
-				}
-			}
-
-		}
-			
-	}
-
 	@IBAction func showHelp(_ sender: Any?) {
 		MacWebBrowser.openURL(URL(string: "https://vincode.io/feed-curator-help/")!, inBackground: false)
 	}
