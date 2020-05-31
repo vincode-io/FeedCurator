63b2b6bf2736f16ceb97853ebfe5de38814ff1b4
Maurice Parker <mo@vincode.io>
2020-05-31 10:45:25 -0500

    Fix error handling description

--- FeedCurator/Extensions/String+.swift
+++ FeedCurator/Extensions/String+.swift
@@ -5,7 +5,7 @@ import Foundation
 // This allows Strings to be directly thrown as errors.  It is a quick and easy way to do
 // what is essentially an untyped error with a message.
 extension String: LocalizedError {
-	public var errorDescription: String? {
+	public var localizedDescription: String {
 		return self
 	}
 }
